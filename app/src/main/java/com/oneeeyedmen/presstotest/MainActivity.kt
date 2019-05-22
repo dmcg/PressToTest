04de4bba38bc95138acf0376e37a5eeccd8b04ed
Duncan McGregor <duncan@oneeyedmen.com>
2019-05-22 12:24:51 +0100

    Change label to button and make it go boom

--- app/src/main/java/com/oneeeyedmen/presstotest/MainActivity.kt
+++ app/src/main/java/com/oneeeyedmen/presstotest/MainActivity.kt
@@ -2,11 +2,19 @@ package com.oneeeyedmen.presstotest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener { view ->
+            Snackbar.make(view, "BOOM!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 }
