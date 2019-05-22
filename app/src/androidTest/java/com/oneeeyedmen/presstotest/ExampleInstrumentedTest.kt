ec1f9e0a1abc925d6917472ec38e2ad632f7d628
Duncan McGregor <duncan@oneeyedmen.com>
2019-05-22 14:36:32 +0100

    Start to test finger down

--- app/src/androidTest/java/com/oneeeyedmen/presstotest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/oneeeyedmen/presstotest/ExampleInstrumentedTest.kt
@@ -1,7 +1,6 @@
 package com.oneeeyedmen.presstotest
 
 import android.view.View
-import androidx.test.InstrumentationRegistry
 import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
@@ -14,9 +13,15 @@ import org.hamcrest.core.AllOf.allOf
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 import org.junit.Rule
-import java.lang.RuntimeException
+import androidx.test.espresso.action.MotionEvents
+import androidx.test.espresso.action.GeneralLocation
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.Press
+import android.view.MotionEvent
+import androidx.test.espresso.UiController
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -36,6 +41,9 @@ class ExampleInstrumentedTest {
                 isDisplayed(),
                 withText("PRESS TO TEST")
             )))
+
+        onView(buttonMatcher).perform(Finger.pressAndHold())
+        onView(buttonMatcher).perform(Finger.release())
     }
 
     @Test
@@ -57,3 +65,59 @@ private val snackBarMatcher = allOf(
     withText("BOOM!")
 )
 
+// https://stackoverflow.com/questions/32010927/android-espresso-make-assertion-while-button-is-kept-pressed
+object Finger {
+    internal var sMotionEventDownHeldView: MotionEvent? = null
+
+    fun pressAndHold(): ViewAction {
+        return PressAndHoldAction()
+    }
+
+    fun release(): ViewAction {
+        return ReleaseAction()
+    }
+
+    fun tearDown() {
+        sMotionEventDownHeldView = null
+    }
+
+    internal class PressAndHoldAction : ViewAction {
+        override fun getConstraints(): Matcher<View> {
+            return isDisplayingAtLeast(90) // Like GeneralClickAction
+        }
+
+        override fun getDescription(): String {
+            return "Press and hold action"
+        }
+
+        override fun perform(uiController: UiController, view: View) {
+            if (sMotionEventDownHeldView != null) {
+                throw AssertionError("Only one view can be held at a time")
+            }
+
+            val precision = Press.FINGER.describePrecision()
+            val coords = GeneralLocation.CENTER.calculateCoordinates(view)
+            sMotionEventDownHeldView = MotionEvents.sendDown(uiController, coords, precision).down
+            // TODO: save view information and make sure release() is on same view
+        }
+    }
+
+    internal class ReleaseAction : ViewAction {
+        override fun getConstraints(): Matcher<View> {
+            return isDisplayingAtLeast(90)  // Like GeneralClickAction
+        }
+
+        override fun getDescription(): String {
+            return "Release action"
+        }
+
+        override fun perform(uiController: UiController, view: View) {
+            if (sMotionEventDownHeldView == null) {
+                throw AssertionError("Before calling release(), you must call pressAndHold() on a view")
+            }
+
+            val coords = GeneralLocation.CENTER.calculateCoordinates(view)
+            MotionEvents.sendUp(uiController, sMotionEventDownHeldView!!, coords)
+        }
+    }
+}
