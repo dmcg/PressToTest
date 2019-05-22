ac5acad3d82463435b065c20413bb66e9d1e7e63
Duncan McGregor <duncan@oneeyedmen.com>
2019-05-22 13:44:50 +0100

    Click button with Espresso

--- app/src/androidTest/java/com/oneeeyedmen/presstotest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/oneeeyedmen/presstotest/ExampleInstrumentedTest.kt
@@ -1,11 +1,16 @@
 package com.oneeeyedmen.presstotest
 
 import androidx.test.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,10 +19,19 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.oneeeyedmen.presstotest", appContext.packageName)
+    }
+
+    @Test
+    fun click_button() {
+        Espresso.onView(withId(R.id.button)).perform(click())
     }
 }
