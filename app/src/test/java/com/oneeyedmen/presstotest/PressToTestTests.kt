9093cdd03c970e51c2cf19bf4b720aeaee3a802e
Duncan McGregor <duncan@oneeyedmen.com>
2019-05-28 14:02:23 +0100

    Unit test beginnings of ViewModel

--- app/src/test/java/com/oneeyedmen/presstotest/PressToTestTests.kt
+++ app/src/test/java/com/oneeyedmen/presstotest/PressToTestTests.kt
@@ -1,30 +1,36 @@
 package com.oneeyedmen.presstotest
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
-import kotlinx.android.synthetic.main.activity_main.*
 import org.junit.Assert.assertEquals
-import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class PressToTestTests {
 
-    @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java)
-
-
-    val buttonText: String get() = activityRule.activity.button.text.toString()
+    val viewModel = ViewModel()
 
     @Test
     fun `button message changes on pressing`() {
-        assertEquals("Press to Test", buttonText)
+        assertEquals("Press to Test", viewModel.buttonText)
 
-//        button.touchButton()
-//        assertEquals("Release to Detonate", buttonText)
+        viewModel.onButtonTouched()
+        assertEquals("Release to Detonate", viewModel.buttonText)
 
-//        untouchButton()
-//        assertEquals("Press to Test", buttonText)
+        viewModel.onButtonUntouched()
+        assertEquals("Press to Test", viewModel.buttonText)
+    }
+}
+
+class ViewModel {
+
+    private val defaultText = "Press to Test"
+    private val pressedText = "Release to Detonate"
+
+    var buttonText = defaultText
+
+    fun onButtonTouched() {
+        buttonText = pressedText
+    }
+
+    fun onButtonUntouched() {
+        buttonText = defaultText
     }
 }
