af9a8eeff25ba3538c8c38cfe82d76d3e59fb486
Duncan McGregor <duncan@oneeyedmen.com>
2019-05-30 14:15:14 +0100

    Trying out Data Binding - get InternalAcceptanceTests running again

--- app/src/test/java/com/oneeyedmen/presstotest/InternalAcceptanceTests.kt
+++ app/src/test/java/com/oneeyedmen/presstotest/InternalAcceptanceTests.kt
@@ -1,7 +1,15 @@
 package com.oneeyedmen.presstotest
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleRegistry
+import org.junit.Before
 import org.robolectric.shadows.ShadowLooper
 
-class InternalAcceptanceTests : AcceptanceTests(robolectricWaiter)
+class InternalAcceptanceTests : AcceptanceTests(robolectricWaiter) {
+    @Before fun hack() {
+        // see https://stackoverflow.com/a/45448554/97777
+        (activityRule.activity.lifecycle as LifecycleRegistry).handleLifecycleEvent(Lifecycle.Event.ON_START)
+    }
+}
 
 private val robolectricWaiter = Waiter(twiddler = { ShadowLooper.runMainLooperToNextTask() })
